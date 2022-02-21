2fd7fe982fa246fe8a306ab76c85aea73a84e59f
Gregory <greg0x46@gmail.com>
2022-02-21 12:28:10 -0300

    start project

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1>About</h1>
+        <p>This is the About page</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
