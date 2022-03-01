c482fc3c406f78cfcc3fa19ccdd9b03337b0994f
Gregory <greg0x46@gmail.com>
2022-03-01 17:43:10 -0300

    parcial commit

--- resources/js/layouts/GuestLayout.vue
+++ resources/js/layouts/GuestLayout.vue
@@ -1,14 +1,15 @@
 <template>
-    <v-app>
-    </v-app>
+  <v-app>
+    <v-main>
+
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
-    name: "GuestLayout"
+    name: "GuestLayout",
 }
 </script>
 
-<style scoped>
 
-</style>
