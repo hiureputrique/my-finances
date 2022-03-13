9f69f8ba9c00dcc3072342f8e80a859fb8e25ca0
hiureputrique <hiureputrique@hotmail.com>
2022-03-13 19:22:54 -0300

    form register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -11,15 +11,48 @@
                         <v-card-subtitle class="text-center">
                             Register in to my finances portal
                         </v-card-subtitle>
-                        <v-form>
-                           <v-text-field prepend-icon="mdi-account" v-model="firstName" :rules="[rules.required]" label="First Name" maxlength="20" required></v-text-field>
-                           <v-text-field prepend-icon="mdi-email" v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-                           <v-text-field prepend-icon="mdi-lock" v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
-                           <v-text-field prepend-icon="mdi-lock" block v-model="verify" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, passwordMatch]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Confirm Password" counter @click:append="show1 = !show1"></v-text-field>
-                             <v-spacer></v-spacer>
-                            <v-btn block dark>
-                                Register
-                            </v-btn>
+                        <v-form @submit.prevent="submit">
+                            <v-text-field
+                                prepend-icon="mdi-account"
+                                v-model="form.name"
+                                :rules="[rules.required]"
+                                label="First Name"
+                                maxlength="20"
+                                required
+                            ></v-text-field>
+                            <v-text-field
+                                prepend-icon="mdi-email"
+                                v-model="form.email"
+                                :rules="emailRules"
+                                label="E-mail"
+                                required
+                            ></v-text-field>
+                            <v-text-field
+                                prepend-icon="mdi-lock"
+                                v-model="form.password"
+                                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                :rules="[rules.required, rules.min]"
+                                :type="show1 ? 'text' : 'password'"
+                                name="input-10-1"
+                                label="Password"
+                                hint="At least 8 characters"
+                                counter
+                                @click:append="show1 = !show1"
+                            ></v-text-field>
+                            <v-text-field
+                                prepend-icon="mdi-lock"
+                                v-model="form.password_confirmation"
+                                block
+                                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                :rules="[rules.required, passwordMatch]"
+                                :type="show1 ? 'text' : 'password'"
+                                name="input-10-1"
+                                label="Confirm Password"
+                                counter
+                                @click:append="show1 = !show1"
+                            ></v-text-field>
+                            <v-spacer></v-spacer>
+                            <v-btn block dark type="submit"> Register </v-btn>
                         </v-form>
                     </v-card-text>
                 </v-card>
@@ -30,51 +63,61 @@
 
 <script>
 import GuestLayout from "../../Layouts/Guest";
-import Logo from "../../Components/Logo"
+import Logo from "../../Components/Logo";
 
 export default {
     name: "Register",
     layout: GuestLayout,
 
     components: {
-        Logo
+        Logo,
     },
     computed: {
-     passwordMatch() {
-      return () => this.password === this.verify || "Password must match";
-    }
-  },    
-    methods: {
-      validate() {
-        if (this.$refs.loginForm.validate()) {
-
-        }
-      },
-      reset() {
-        this.$refs.form.reset();
-      },
-      resetValidation() {
-        this.$refs.form.resetValidation();
-      }
+        passwordMatch() {
+            return () =>
+                this.password === this.password_confirmation ||
+                "Password must match";
+        },
     },
-      data: () => ({
-      dialog: true,
-      valid: true,
-      name: "",
-      email: "",
-      password: "",
-      verify: "",
-      emailRules: [
-        v => !!v || "Required",
-        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-      ],
-      show1: false,
-      rules: {
-        required: value => !!value || "Required.",
-        min: v => (v && v.length >= 8) || "Min 8 characters"
-      }
-    })
-}
+    props: {
+        errors: Object,
+    },
+    data: () => ({
+        dialog: true,
+        valid: true,
+        form: {
+            name: null,
+            email: null,
+            password: null,
+            password_confirmation: null,
+        },
+        emailRules: [
+            (v) => !!v || "Required",
+            (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+        ],
+        show1: false,
+        rules: {
+            required: (value) => !!value || "Required.",
+            min: (v) => (v && v.length >= 8) || "Min 8 characters",
+        },
+    }),
+    methods: {
+        validate() {
+            if (this.$refs.loginForm.validate()) {
+            }
+        },
+        reset() {
+            this.$refs.form.reset();
+        },
+        resetValidation() {
+            this.$refs.form.resetValidation();
+        },
+        submit() {
+            //  name: this.name
+            this.$inertia.post("/register", this.form);
+        },
+    },
+};
 </script>
 
 <style scoped>
